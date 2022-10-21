b4274b5b843fc0ce56722ba6cbf286938153c375
Sparrow0hawk <alexjcoleman@gmail.com>
2022-10-21 20:36:28 +0100

    update main to use rebuild site that returns empty OK

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-mod templates
+mod templates;
+use oxysite::rebuild_site;
 
 const CONTENT_DIR: &str = "content";
 const PUBLIC_DIR: &str = "public";
 
 fn main() -> Result<(), anyhow::Error> {
-    rebuild_site(CONTENT_DIR, PUBLIC_DIR).expect("Building site");
+    rebuild_site(CONTENT_DIR, PUBLIC_DIR);
 
     // rebuild_site(&str, &str)
     // delete public dir
@@ -13,4 +14,5 @@ fn main() -> Result<(), anyhow::Error> {
     // read file
     // parse content and pass it into read_body func in templates
     // save templated file into output dir
+    Ok(())
 }
