2cf44a4a2d9c71b16102c24211c5ad2624d96414
Sparrow0hawk <alexjcoleman@gmail.com>
2022-10-25 09:05:14 +0100

    add logic for generating site

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
-use log::info;
-use std::fs::create_dir;
+use anyhow;
+use pulldown_cmark;
+use std::fs;
 use std::path::Path;
 use walkdir::WalkDir;
 
-pub fn rebuild_site(content_dir: &str, build_dir: &str) -> Vec<String> {
+mod templates;
+
+pub fn rebuild_site(content_dir: &str, build_dir: &str) -> Result<Vec<String>, anyhow::Error> {
     let _ = build_dir_control(build_dir);
     let mut content_files: Vec<String> = WalkDir::new(content_dir)
         .into_iter()
@@ -12,9 +15,28 @@ pub fn rebuild_site(content_dir: &str, build_dir: &str) -> Vec<String> {
         .map(|e| e.path().display().to_string())
         .collect();
 
-    println!("{:?}", content_files);
+    let mut html_files = Vec::with_capacity(content_files.len());
 
-    content_files
+    for file in &content_files {
+        let mut html = templates::HEADER.to_owned();
+        let markdown_file = fs::read_to_string(&file)?;
+        let parser =
+            pulldown_cmark::Parser::new_ext(&markdown_file, pulldown_cmark::Options::all());
+        let mut body = String::new();
+
+        pulldown_cmark::html::push_html(&mut body, parser);
+
+        html.push_str(templates::render_body(&body).as_str());
+        html.push_str(templates::FOOTER);
+
+        let html_file = file.replace(content_dir, build_dir).replace(".md", ".html");
+
+        html_files.push(html_file);
+    }
+
+    println!("{:?}", html_files);
+
+    Ok(html_files)
 }
 
 fn build_dir_control(build_dir: &str) -> () {
