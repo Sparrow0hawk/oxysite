172510bf38718d121c608fe08ae5f752d467e8fd
Sparrow0hawk <alexjcoleman@gmail.com>
2022-10-30 19:48:14 +0000

    added tests and started build_page function

--- src/lib.rs
+++ src/lib.rs
@@ -21,11 +21,12 @@ impl Site {
         })
     }
 }
-pub fn rebuild_site(site: Site) -> Result<Vec<String>, anyhow::Error> {
-    let site: Site = site;
 
-    let _ = build_dir_control(&site.build_dir);
-    let mut content_files: Vec<String> = WalkDir::new(&site.content_dir)
+pub fn rebuild_site(site: Site) -> Result<Vec<String>, anyhow::Error> {
+    let config: Site = site;
+
+    let _ = build_dir_control(&config.build_dir);
+    let mut content_files: Vec<String> = WalkDir::new(&config.content_dir)
         .into_iter()
         .filter_map(|x| x.ok())
         .filter(|file| file.path().display().to_string().ends_with(".md"))
@@ -35,23 +36,14 @@ pub fn rebuild_site(site: Site) -> Result<Vec<String>, anyhow::Error> {
     let mut html_files = Vec::with_capacity(content_files.len());
 
     for file in &content_files {
-        let mut html = templates::HEADER.to_owned();
-        let markdown_file = fs::read_to_string(&file)?;
-        let parser =
-            pulldown_cmark::Parser::new_ext(&markdown_file, pulldown_cmark::Options::all());
-        let mut body = String::new();
+        let html_name;
 
-        pulldown_cmark::html::push_html(&mut body, parser);
+        html_name = build_md(&file, &config).unwrap_or_else(|e| {
+            error!("An error occured building html from markdown {}", e);
+            process::exit(1)
+        });
 
-        html.push_str(templates::render_body(&body).as_str());
-        html.push_str(templates::FOOTER);
-
-        let html_file = file.replace(content_dir, build_dir).replace(".md", ".html");
-        let folder = Path::new(&html_file).parent().unwrap();
-        let _ = fs::create_dir_all(folder);
-        fs::write(&html_file, html)?;
-
-        html_files.push(html_file);
+        html_files.push(html_name);
     }
 
     println!("{:?}", html_files);
@@ -72,6 +64,34 @@ fn build_dir_control(build_dir: &str) -> () {
     }
 }
 
+fn build_md(file: &str, site: &Site) -> Result<String, std::io::Error> {
+    // build html files from markdown files
+    // uses pulldown_cmark crate to do conversion
+    // creates a
+    let mut html = templates::HEADER.to_owned();
+    let markdown_file = fs::read_to_string(&file)?;
+    let parser = pulldown_cmark::Parser::new_ext(&markdown_file, pulldown_cmark::Options::all());
+    let mut body = String::new();
+
+    pulldown_cmark::html::push_html(&mut body, parser);
+
+    html.push_str(templates::render_body(&body).as_str());
+    html.push_str(templates::FOOTER);
+
+    let html_file = file
+        .replace(&site.content_dir, &site.build_dir)
+        .replace(".md", ".html");
+    let folder = Path::new(&html_file).parent().unwrap();
+    let _ = fs::create_dir_all(folder);
+    fs::write(&html_file, html)?;
+
+    Ok(html_file)
+}
+
+fn build_page(markdown_file: &str) -> String {
+    markdown_file.to_string()
+}
+
 #[cfg(test)]
 
 mod test {
@@ -90,11 +110,49 @@ mod test {
 
         assert_eq!("public/home.html", html_file)
     }
+
+    #[test]
+    fn test_build_page() {
+        let md = "# Blog page\n- item 1\n- item 2\n- item 3".to_string();
+
+        let output = build_page(&md);
+
+        assert_eq!(
+            // assert returned string matches string
+            output,
+            r#"<!DOCTYPE html>
+            <html lang="en">
+            
+                <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width", initial-scale=1">
+                </head>
+                <h1>Blog page</h1>
+                <ul>
+                <li>item 1</li>
+                <li>item 2</li>
+                <li>item 3</li>
+                </ul>
+                </body>
+                </html>
+            "#
+            .to_string()
+        )
+    }
+
     #[test]
     fn rebuild_basic_test() {
+        let test_site = Site {
+            content_dir: String::from("test_content"),
+            build_dir: String::from("public"),
+        };
+
+        let _ = fs::remove_dir_all("public");
+
         assert_eq!(
-            rebuild_site("test_content", "test_build").unwrap(),
-            vec!["test_build/blog.html", "test_build/home.html"]
+            // this should check fiel system for files
+            rebuild_site(test_site).unwrap(),
+            vec!["public/blog.html", "public/home.html"]
         )
     }
 }
