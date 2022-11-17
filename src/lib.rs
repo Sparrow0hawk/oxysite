626992e59ca7780e202d862435cb0b65288f9f33
Sparrow0hawk <alexjcoleman@gmail.com>
2022-11-17 20:40:37 +0000

    add write_html function
    that writes html string to file and includes a test

--- src/lib.rs
+++ src/lib.rs
@@ -43,6 +43,11 @@ pub fn rebuild_site(site: Site) -> Result<Vec<String>, anyhow::Error> {
             process::exit(1)
         });
 
+        let html_name = write_html(&file, html, &config).unwrap_or_else(|e| {
+            error!("An error occured writing html files to disk: {}", e);
+            process::exit(1);
+        });
+
         html_files.push(html_name);
     }
 
@@ -79,6 +84,8 @@ fn build_md(file: &str) -> Result<String, anyhow::Error> {
 
     Ok(html)
 }
+
+fn write_html(file: &str, html: String, site: &Site) -> Result<String, std::io::Error> {
     let html_file = file
         .replace(&site.content_dir, &site.build_dir)
         .replace(".md", ".html");
@@ -89,10 +96,6 @@ fn build_md(file: &str) -> Result<String, anyhow::Error> {
     Ok(html_file)
 }
 
-fn build_page(markdown_file: &str) -> String {
-    markdown_file.to_string()
-}
-
 #[cfg(test)]
 
 mod test {
@@ -100,15 +103,28 @@ mod test {
     use assert_str::assert_str_trim_eq;
 
     #[test]
-    fn test_build_md() {
+    fn test_write_html() {
         let file = "test_content/home.md";
         let test_site = Site {
             content_dir: String::from("test_content"),
             build_dir: String::from("public"),
         };
 
+        let test_html = r#"<!DOCTYPE html>
+        <html lang="en">
+        
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width", initial-scale=1">
+            </head>
+            <h1>Hello world!</h1>
+            </body>
+            </html>
+        "#
+        .to_string();
+
         let _ = fs::remove_dir_all("public");
-        let html_file = build_md(file, &test_site).unwrap();
+        let html_file = write_html(file, test_html, &test_site).unwrap();
 
         assert_eq!("public/home.html", html_file)
     }
